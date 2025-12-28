95308d8ae179e5831155972d4d9014932cbf8e0a
Thareesha <thareesha98@gmail.com>
2025-12-28 17:03:52 +0530

    Add RabbitMQ configuration

---
+++ sbms-backend/src/main/java/com/sbms/sbms_backend/config/RabbitMQConfig.java
@@ -0,0 +1,49 @@
+package com.sbms.sbms_backend.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${sbms.rabbitmq.exchange:sbms.events}")
+    private String exchangeName;
+
+    // global ObjectMapper (configured for Java Time)
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper;
+    }
+
+    @Bean
+    public TopicExchange eventExchange() {
+        return new TopicExchange(exchangeName, true, false);
+    }
+
+    // Use Jackson2JsonMessageConverter without deprecated constructors.
+    // It will use Spring's shared ObjectMapper if present.
+    @Bean
+    public MessageConverter jsonMessageConverter(ObjectMapper objectMapper) {
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+                                         MessageConverter messageConverter) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter);
+        return template;
+    }
+}

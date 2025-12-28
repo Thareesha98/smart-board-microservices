c0f13eb7c48b5c41faea6c804054938b2e50a0bb
Thareesha <thareesha98@gmail.com>
2025-12-28 12:14:52 +0530

    Add SbmsBackendApplication entry point

---
+++ sbms-backend/src/main/java/com/sbms/sbms_backend/SbmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sbms.sbms_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbmsBackendApplication.class, args);
+	}
+
+}

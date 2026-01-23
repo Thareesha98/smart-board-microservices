1311bb37ad811519503a751cd925e7eea01e75d2
Thareesha <thareesha98@gmail.com>
2026-01-23 15:26:22 +0530

    Add PageResponse DTO

---
+++ boarding-service/src/main/java/com/sbms/boarding_service/dto/common/PageResponse.java
@@ -0,0 +1,19 @@
+package com.sbms.boarding_service.dto.common;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class PageResponse<T> {
+
+    private List<T> content;
+    private int page;
+    private int size;
+    private long totalElements;
+    private int totalPages;
+    private boolean first;
+    private boolean last;
+}

2dc49ecefa9357c1c9fa8ebb34c9adb0a776eb95
Thareesha <thareesha98@gmail.com>
2026-01-30 23:09:34 +0530

    dto: add MaintenanceResponseDTO

---
+++ sbms-maintenance-service/src/main/java/com/sbms/sbms_maintenance_service/dto/maintenance/MaintenanceResponseDTO.java
@@ -0,0 +1,28 @@
+package com.sbms.sbms_maintenance_service.dto.maintenance;
+
+
+import java.util.List;
+
+import com.sbms.sbms_maintenance_service.model.enums.MaintenanceStatus;
+
+import lombok.Data;
+
+@Data
+public class MaintenanceResponseDTO {
+
+    private Long id;
+
+    private Long boardingId;
+    private String boardingTitle;
+    private Long studentId;
+    private String studentName;
+
+    private String title;
+    private String description;
+    private List<String> imageUrls;
+
+    private MaintenanceStatus status;
+
+    private String studentNote;
+    private String ownerNote;
+}

8819eb638689911f86f20cc2592d7da38b30da50
Thareesha <thareesha98@gmail.com>
2026-01-30 23:09:48 +0530

    feat: maintenance service main entry point

---
+++ sbms-maintenance-service/src/main/java/com/sbms/sbms_maintenance_service/SbmsMaintenanceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sbms.sbms_maintenance_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbmsMaintenanceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbmsMaintenanceServiceApplication.class, args);
+	}
+
+}

35b7b3cc565eacef43f3bfaa018612425f23c943
Thareesha <thareesha98@gmail.com>
2026-02-06 23:52:19 +0530

    refactor: update RegistrationMapper

--- sbms-backend/src/main/java/com/sbms/sbms_backend/mapper/RegistrationMapper.java
+++ sbms-backend/src/main/java/com/sbms/sbms_backend/mapper/RegistrationMapper.java
@@ -2,40 +2,56 @@ package com.sbms.sbms_backend.mapper;
 
 import java.math.BigDecimal;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.sbms.sbms_backend.client.UserClient;
 import com.sbms.sbms_backend.dto.registration.RegistrationResponseDTO;
+import com.sbms.sbms_backend.dto.user.UserMinimalDTO;
+import com.sbms.sbms_backend.model.PaymentIntent;
 import com.sbms.sbms_backend.model.Registration;
+import com.sbms.sbms_backend.repository.PaymentIntentRepository;
+
+
 
 public class RegistrationMapper {
-
+	
+	@Autowired UserClient userClient;
+	
     public static RegistrationResponseDTO toDTO(
-            Registration r,
-            String boardingTitle,
-            BigDecimal keyMoney,
-            BigDecimal monthlyPrice
-    ) {
-
+    		Registration r, 
+    		BigDecimal keyMoney,
+    	    BigDecimal monthlyPrice,
+    	    UserMinimalDTO user
+    	  
+    	    
+    		
+    		) {
         RegistrationResponseDTO dto = new RegistrationResponseDTO();
 
-        // Registration
         dto.setId(r.getId());
+
         dto.setBoardingId(r.getBoardingId());
-        dto.setBoardingTitle(boardingTitle);
+        //dto.setBoardingTitle(r.getBoardingTitle());
 
-        // Student
-        dto.setStudentId(r.getStudent().getId());
-        dto.setStudentName(r.getStudent().getFullName());
-        dto.setStudentEmail(r.getStudent().getEmail());
+        dto.setStudentId(r.getStudentId());
+        dto.setStudentName(user.getFullName());
+        dto.setStudentEmail(user.getEmail());
 
-        // Registration info
         dto.setNumberOfStudents(r.getNumberOfStudents());
         dto.setStatus(r.getStatus());
         dto.setStudentNote(r.getStudentNote());
         dto.setOwnerNote(r.getOwnerNote());
 
-        // Payment
         dto.setKeyMoney(keyMoney);
         dto.setMonthlyPrice(monthlyPrice);
         dto.setKeyMoneyPaid(r.isKeyMoneyPaid());
+        dto.setPaymentSlipUrl(r.getPaymentTransactionRef());
+        dto.setAgreementPdfPath(r.getAgreementPdfPath());
+
+        
+        dto.setPaymentMethod(r.getPaymentMethod());
+
 
         return dto;
     }

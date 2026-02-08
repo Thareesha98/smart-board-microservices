08e6c80c722a704f7fc19d1634cab6d2e32c150b
Thareesha <thareesha98@gmail.com>
2026-02-08 23:38:24 +0530

    feat: add external service clients to boarding-service

---
+++ boarding-service/src/main/java/com/sbms/boarding_service/client/UserClient.java
@@ -0,0 +1,23 @@
+package com.sbms.boarding_service.client;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.sbms.boarding_service.dto.common.UserMinimalDTO;
+
+@Service
+public class UserClient {
+    private final WebClient webClient;
+
+    public UserClient(WebClient.Builder builder) {
+        this.webClient = builder.baseUrl("http://user-service:8080").build();
+    }
+
+    public UserMinimalDTO getUserMinimal(Long userId) {
+        return webClient.get()
+                .uri("/api/user/internal/users/{id}/minimal", userId)
+                .retrieve()
+                .bodyToMono(UserMinimalDTO.class)
+                .block();
+    }
+}

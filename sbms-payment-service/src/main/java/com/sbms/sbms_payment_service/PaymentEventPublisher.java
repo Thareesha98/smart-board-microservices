91fd26b4c323f164370995e937b00c4f9b436261
Thareesha <thareesha98@gmail.com>
2026-02-08 23:39:49 +0530

    feat: implement payment event publisher

---
+++ sbms-payment-service/src/main/java/com/sbms/sbms_payment_service/PaymentEventPublisher.java
@@ -0,0 +1,40 @@
+package com.sbms.sbms_payment_service;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+import com.sbms.sbms_payment_service.events.PaymentFailedEvent;
+import com.sbms.sbms_payment_service.events.PaymentSucceededEvent;
+
+import org.springframework.transaction.event.TransactionPhase;
+
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class PaymentEventPublisher {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void onPaymentSuccess(PaymentSucceededEvent event) {
+
+        rabbitTemplate.convertAndSend(
+                "payment.exchange",
+                "payment.succeeded",
+                event.paymentId()   // IDENTITY ONLY
+        );
+    }
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void onPaymentFailed(PaymentFailedEvent event) {
+
+        rabbitTemplate.convertAndSend(
+                "payment.exchange",
+                "payment.failed",
+                event.paymentId()
+        );
+    }
+}

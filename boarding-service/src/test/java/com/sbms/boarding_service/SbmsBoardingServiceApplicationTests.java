fe3ad8bb453a76f40a95469f09c71f907648739f
Thareesha <thareesha98@gmail.com>
2026-02-08 23:38:31 +0530

    test: add initial test suite for boarding-service

---
+++ boarding-service/src/test/java/com/sbms/boarding_service/SbmsBoardingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbms.boarding_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbmsBoardingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

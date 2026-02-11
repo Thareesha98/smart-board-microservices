feccfd621f65be75125db1d37247edb72bdb1c50
Thareesha <thareesha98@gmail.com>
2026-02-11 23:58:26 +0530

    feat: update ReportService application bootstrap

---
+++ sbms-report-service/src/main/java/com/sbms/sbms_report_service/SbmsReportServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sbms.sbms_report_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbmsReportServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbmsReportServiceApplication.class, args);
+	}
+
+}

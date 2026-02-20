16120d0c344b4e327391090909e3728884f0fe12
Thareesha <thareesha98@gmail.com>
2026-02-20 23:39:21 +0530

    Update UserClient

--- sbms-payment-service/src/main/java/com/sbms/sbms_payment_service/client/UserClient.java
+++ sbms-payment-service/src/main/java/com/sbms/sbms_payment_service/client/UserClient.java
@@ -4,26 +4,45 @@ package com.sbms.sbms_payment_service.client;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import com.sbms.sbms_payment_service.dto.UserBasicInfo;
+import com.sbms.sbms_payment_service.dto.user.UserMinimalDTO;
 
+import java.time.Duration;
+
+
+
+import reactor.core.publisher.Mono;
 
 @Service
 public class UserClient {
 
     private final WebClient webClient;
 
-    public UserClient(WebClient.Builder builder) {
-        this.webClient = builder
-                .baseUrl("http://user-service:8080")
-                .build();
+    public UserClient(WebClient userServiceWebClient) {
+        this.webClient = userServiceWebClient;
     }
 
-    public UserBasicInfo getUser(Long userId) {
+    public UserMinimalDTO findByEmail(String email) {
         return webClient.get()
-                .uri("/api/users/internal/{id}/basic", userId)
+                .uri("/api/internal/users/by-email?email={email}", email)
                 .retrieve()
-                .bodyToMono(UserBasicInfo.class)
-                .timeout(java.time.Duration.ofSeconds(2))
+                .onStatus(
+                        status -> status.is4xxClientError(),
+                        response -> Mono.error(new RuntimeException("User not found"))
+                )
+                .onStatus(
+                        status -> status.is5xxServerError(),
+                        response -> Mono.error(new RuntimeException("User service down"))
+                )
+                .bodyToMono(UserMinimalDTO.class)
+                .timeout(Duration.ofSeconds(3))
+                .block();
+    }
+
+    public UserMinimalDTO getUserMinimal(Long userId) {
+        return webClient.get()
+                .uri("/api/internal/users/{id}/minimal", userId)
+                .retrieve()
+                .bodyToMono(UserMinimalDTO.class)
                 .block();
     }
 }

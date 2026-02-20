60257864ade8856e78f2fd7745de34a5e96014f6
Thareesha <thareesha98@gmail.com>
2026-02-20 23:43:00 +0530

    Update PaymentTransaction entity

---
+++ sbms-payment-service/src/main/java/com/sbms/sbms_payment_service/entity/PaymentTransaction.java
@@ -0,0 +1,59 @@
+package com.sbms.sbms_payment_service.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import com.sbms.sbms_notification_service.model.enums.PaymentMethod;
+import com.sbms.sbms_notification_service.model.enums.PaymentStatus;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class PaymentTransaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String transactionRef;
+
+    @ManyToOne(optional = false)
+    private PaymentIntent intent;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod method;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus status;
+
+    @Column(nullable = false, precision = 12, scale = 2)
+    private BigDecimal amount; // gross amount
+
+    //  COMMISSION BREAKDOWN
+    @Column(precision = 12, scale = 2)
+    private BigDecimal platformFee;
+
+    @Column(precision = 12, scale = 2)
+    private BigDecimal gatewayFee;
+
+    @Column(precision = 12, scale = 2)
+    private BigDecimal netAmount; // owner receives
+
+    private String gateway;
+    private String failureReason;
+
+    private LocalDateTime paidAt;
+    private String receiptPath;
+    
+    
+    
+    @Column(length = 500)
+    private String slipUrl; 
+    
+    private LocalDateTime verifiedAt;
+    private Long verifiedByOwnerId;
+}

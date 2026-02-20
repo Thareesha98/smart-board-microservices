55bb85704f27fe24effb62e84346c048659fe773
Thareesha <thareesha98@gmail.com>
2026-02-20 23:39:32 +0530

    Update PaymentIntentRepository

--- sbms-payment-service/src/main/java/com/sbms/sbms_payment_service/repository/PaymentIntentRepository.java
+++ sbms-payment-service/src/main/java/com/sbms/sbms_payment_service/repository/PaymentIntentRepository.java
@@ -1,16 +1,37 @@
 package com.sbms.sbms_payment_service.repository;
 
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.sbms.sbms_notification_service.model.enums.PaymentIntentStatus;
+import com.sbms.sbms_notification_service.model.enums.PaymentType;
 import com.sbms.sbms_payment_service.entity.PaymentIntent;
-
-import java.util.Optional;
 
 public interface PaymentIntentRepository extends JpaRepository<PaymentIntent, Long> {
 
-    Optional<PaymentIntent> findByReferenceTypeAndReferenceId(
-            String referenceType,
-            String referenceId
+    List<PaymentIntent> findByStudentId(Long studentId);
+
+    List<PaymentIntent> findByBoardingId(Long boardingId);
+    
+    List<PaymentIntent> findByOwnerId(Long boardingId);
+    
+    Optional<PaymentIntent> findByMonthlyBillId(Long monthlyBillId);
+
+  
+    
+    Optional<PaymentIntent> findByStudentIdAndBoardingIdAndType(
+            Long studentId,
+            Long boardingId,
+            PaymentType type
     );
+    List<PaymentIntent> findByStatus(PaymentIntentStatus status);
+    
+    Optional<PaymentIntent> findTopByStudentIdAndBoardingIdAndTypeOrderByCreatedAtDesc(
+            Long studentId,
+            Long boardingId,
+            PaymentType type
+    );
+
 }

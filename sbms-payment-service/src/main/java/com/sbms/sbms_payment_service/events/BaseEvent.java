5b195267adb76dc52f19c539709045a9d72dd2a1
Thareesha <thareesha98@gmail.com>
2026-02-20 23:43:03 +0530

    Update BaseEvent

---
+++ sbms-payment-service/src/main/java/com/sbms/sbms_payment_service/events/BaseEvent.java
@@ -0,0 +1,25 @@
+package com.sbms.sbms_payment_service.events;
+
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public abstract class BaseEvent {
+
+    private String eventId;
+    private String eventType;
+    private LocalDateTime occurredAt;
+
+    protected BaseEvent(String eventType) {
+        this.eventId = UUID.randomUUID().toString();
+        this.eventType = eventType;
+        this.occurredAt = LocalDateTime.now();
+    }
+}

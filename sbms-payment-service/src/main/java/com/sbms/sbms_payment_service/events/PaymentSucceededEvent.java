c9d5ead9f1677620871a23cfe8977bd3359fced8
Thareesha <thareesha98@gmail.com>
2026-02-20 23:39:29 +0530

    Update PaymentSucceededEvent

--- sbms-payment-service/src/main/java/com/sbms/sbms_payment_service/events/PaymentSucceededEvent.java
+++ sbms-payment-service/src/main/java/com/sbms/sbms_payment_service/events/PaymentSucceededEvent.java
@@ -1,12 +1,31 @@
 package com.sbms.sbms_payment_service.events;
 
+import java.math.BigDecimal;
 
-public record PaymentSucceededEvent(
-        Long paymentId,
-        Long paymentIntentId,
-        Long studentId,
-        Long ownerId,
-        Long boardingId,
-        String referenceType,
-        String referenceId
-) {}
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+
+
+
+
+
+@Getter
+@Setter
+public class PaymentSucceededEvent extends BaseEvent {
+
+    private Long intentId;
+    private Long transactionId;
+    private Long studentId;
+    private Long ownerId;
+    private Long monthlyBillId;
+    private BigDecimal amount;
+    private String method;
+    private String transactionRef;
+
+    public PaymentSucceededEvent() {
+        super("payment.succeeded");
+    }
+}

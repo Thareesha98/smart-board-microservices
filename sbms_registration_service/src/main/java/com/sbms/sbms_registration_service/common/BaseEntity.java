437b4b047c8649adf5f1c75e5e257408a7e88c85
Thareesha <thareesha98@gmail.com>
2026-02-25 23:35:09 +0530

    Update BaseEntity for common auditing

---
+++ sbms_registration_service/src/main/java/com/sbms/sbms_registration_service/common/BaseEntity.java
@@ -0,0 +1,34 @@
+package com.sbms.sbms_registration_service.common;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    @Column(updatable = false)
+    protected LocalDateTime createdAt;
+
+    @Column
+    protected LocalDateTime updatedAt;
+
+    @PrePersist
+    public void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = createdAt;
+    }
+
+    @PreUpdate
+    public void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+}

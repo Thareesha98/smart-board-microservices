12d8f2f16187f8031a8a9640f9f5e4152b77ef9c
Thareesha <thareesha98@gmail.com>
2026-02-25 23:35:42 +0530

    Update main Application class

---
+++ sbms_registration_service/src/main/java/com/sbms/sbms_registration_service/SbmsRegistrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sbms.sbms_registration_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbmsRegistrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbmsRegistrationServiceApplication.class, args);
+	}
+
+}

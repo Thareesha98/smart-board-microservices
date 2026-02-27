e86ca48528634dbaf271bff9a78bb110c89e0879
Thareesha <thareesha98@gmail.com>
2026-02-27 23:55:20 +0530

    Refactor WebClient configuration

--- sbms-payment-service/src/main/java/com/sbms/sbms_payment_service/client/WebClientConfig.java
+++ sbms-payment-service/src/main/java/com/sbms/sbms_payment_service/client/WebClientConfig.java
@@ -4,41 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
+/**
+ * GLOBAL WebClient configuration.
+ * We expose ONLY the Builder to avoid bean conflicts in microservices.
+ * Each client will set its own baseUrl (BEST PRACTICE in Kubernetes).
+ */
 @Configuration
 public class WebClientConfig {
 
     @Bean
-    public WebClient userServiceWebClient(WebClient.Builder builder) {
-        return builder
-                .baseUrl("http://user-service:8080")
-                .build();
-    }
-
-    @Bean
-    public WebClient emailServiceWebClient(WebClient.Builder builder) {
-        return builder
-                .baseUrl("http://user-service:8080")
-                .build();
-    }
-
-    @Bean
-    public WebClient boardingServiceWebClient(WebClient.Builder builder) {
-        return builder
-                .baseUrl("http://boarding-service:8080")
-                .build();
-    }
-
-    @Bean
-    public WebClient registrationServiceWebClient(WebClient.Builder builder) {
-        return builder
-                .baseUrl("http://registration-service:8080")
-                .build();
-    }
-
-    @Bean
-    public WebClient fileServiceWebClient(WebClient.Builder builder) {
-        return builder
-                .baseUrl("http://sbms-backend:8080")
-                .build();
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
     }
 }

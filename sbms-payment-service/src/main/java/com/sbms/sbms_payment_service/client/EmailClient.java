269f243bec832b07eed14e8a001e780dfc16c0d5
Thareesha <thareesha98@gmail.com>
2026-02-27 23:55:24 +0530

    Update Email notification client

--- sbms-payment-service/src/main/java/com/sbms/sbms_payment_service/client/EmailClient.java
+++ sbms-payment-service/src/main/java/com/sbms/sbms_payment_service/client/EmailClient.java
@@ -1,16 +1,13 @@
 package com.sbms.sbms_payment_service.client;
 
-
 import java.time.Duration;
 import java.util.Base64;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.sbms.sbms_payment_service.dto.user.SendEmailRequest;
-
 
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
@@ -21,13 +18,12 @@ public class EmailClient {
 
     private final WebClient webClient;
 
-   
-    public EmailClient(
-            @Qualifier("emailServiceWebClient") WebClient webClient
-    ) {
-        this.webClient = webClient;
+    public EmailClient(WebClient.Builder builder) {
+        this.webClient = builder
+                .baseUrl("http://user-service:8080")
+                .build();
     }
-    
+
     @Retry(name = "emailService", fallbackMethod = "fallbackEmail")
     public void sendPaymentReceipt(
             String email,
@@ -35,15 +31,17 @@ public class EmailClient {
             String receiptNumber,
             byte[] pdfBytes
     ) {
+
         SendEmailRequest req = new SendEmailRequest();
         req.setType("PAYMENT_RECEIPT");
         req.setTo(email);
-
         req.setData(Map.of(
                 "studentName", studentName,
                 "receiptNumber", receiptNumber,
                 "pdfBytes", Base64.getEncoder().encodeToString(pdfBytes)
         ));
+
+        log.info("Sending receipt email to {}", email);
 
         webClient.post()
                 .uri("/api/internal/email/send")
@@ -62,6 +60,5 @@ public class EmailClient {
             Throwable ex
     ) {
         log.error("Email service DOWN. Receipt email skipped for {}", email, ex);
-        // DO NOTHING — payment must succeed even if email fails
     }
 }

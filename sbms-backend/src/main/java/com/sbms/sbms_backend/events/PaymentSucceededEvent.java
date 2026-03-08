0f512e547523300fc13fbe45e00e8b6783d21f5f
Thareesha <thareesha98@gmail.com>
2026-03-08 23:54:45 +0530

    feat(backend): update payment succeeded event structure

--- sbms-backend/src/main/java/com/sbms/sbms_backend/events/PaymentSucceededEvent.java
+++ sbms-backend/src/main/java/com/sbms/sbms_backend/events/PaymentSucceededEvent.java
@@ -3,6 +3,8 @@ package com.sbms.sbms_backend.events;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +12,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class PaymentSucceededEvent {
     private String eventId;
     private Long intentId;

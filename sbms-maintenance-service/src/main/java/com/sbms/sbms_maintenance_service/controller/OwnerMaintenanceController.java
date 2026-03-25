8909d0a345c8b9e45f77b5b7850c141f0b4d468f
Thareesha <thareesha98@gmail.com>
2026-03-26 00:07:51 +0530

    Update OwnerMaintenanceController

--- sbms-maintenance-service/src/main/java/com/sbms/sbms_maintenance_service/controller/OwnerMaintenanceController.java
+++ sbms-maintenance-service/src/main/java/com/sbms/sbms_maintenance_service/controller/OwnerMaintenanceController.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sbms.sbms_maintenance_service.dto.maintenance.MaintenanceDecisionDTO;
@@ -37,5 +39,35 @@ public class OwnerMaintenanceController {
             @RequestBody MaintenanceDecisionDTO dto
     ) {
         return service.decide(ownerId, id, dto);
+    }
+    
+    
+    
+    @GetMapping("/technician")
+    public List<MaintenanceResponseDTO> technicianJobs(
+            @RequestHeader("X-User-Id") Long technicianId
+    ) {
+        return service.getForTechnician(technicianId);
+    }
+    
+    
+    
+    
+    @PutMapping("/{maintenanceId}/assign/{technicianId}")
+    public String assignTechnician(
+            @RequestHeader("X-User-Id") Long ownerId,
+            @PathVariable Long maintenanceId,
+            @PathVariable Long technicianId) {
+        service.assignTechnician(ownerId, maintenanceId, technicianId);
+        return "Assigned successfully.";
+    }
+
+    @PostMapping("/{maintenanceId}/review")
+    public MaintenanceResponseDTO reviewTechnician(
+            @RequestHeader("X-User-Id") Long ownerId,
+            @PathVariable Long maintenanceId,
+            @RequestParam int rating,
+            @RequestParam String comment) {
+        return service.reviewTechnician(ownerId, maintenanceId, rating, comment);
     }
 }

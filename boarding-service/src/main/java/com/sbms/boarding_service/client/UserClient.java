bda8abb127c19c062d423814a7c72baceebddd1c
Thareesha <thareesha98@gmail.com>
2026-03-25 23:58:50 +0530

    Update UserClient in boarding-service

--- boarding-service/src/main/java/com/sbms/boarding_service/client/UserClient.java
+++ boarding-service/src/main/java/com/sbms/boarding_service/client/UserClient.java
@@ -1,9 +1,14 @@
 package com.sbms.boarding_service.client;
 
+import java.time.Duration;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import com.sbms.boarding_service.dto.boarding.UserSnapshotDTO;
 import com.sbms.boarding_service.dto.common.UserMinimalDTO;
+
+import reactor.core.publisher.Mono;
 
 
 @Service
@@ -24,4 +29,27 @@ public class UserClient {
                 .bodyToMono(UserMinimalDTO.class)
                 .block(); // OK for internal sync calls
     }
+    
+    
+    public UserSnapshotDTO getUserSnapshot(Long userId) {
+
+        return webClient.get()
+                .uri("/api/internal/users/{id}/snapshot", userId)
+                .retrieve()
+                .onStatus(
+                        status -> status.is4xxClientError(),
+                        response -> Mono.error(new RuntimeException("User not found"))
+                    )
+                    .onStatus(
+                        status -> status.is5xxServerError(),
+                        response -> Mono.error(new RuntimeException("User service down"))
+                    )
+                .bodyToMono(UserSnapshotDTO.class)
+                .timeout(Duration.ofSeconds(3))
+                .block();
+    }
+    
+    
+    
+    
 }

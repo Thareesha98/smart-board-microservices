706976460ed639dae0ef94eb5e0ff00e4faf8494
Thareesha <thareesha98@gmail.com>
2026-03-25 23:58:57 +0530

    Update OwnerBoardingController

--- boarding-service/src/main/java/com/sbms/boarding_service/controller/OwnerBoardingController.java
+++ boarding-service/src/main/java/com/sbms/boarding_service/controller/OwnerBoardingController.java
@@ -12,7 +12,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/boardings/owner")
-@CrossOrigin
 @Tag(
 	    name = "Owner Boardings",
 	    description = "APIs for boarding owners to manage their boardings"

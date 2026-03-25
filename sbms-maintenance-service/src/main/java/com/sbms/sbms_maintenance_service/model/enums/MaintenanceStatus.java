4cfdbeec42877436b848579156047ff77e4fd195
Thareesha <thareesha98@gmail.com>
2026-03-26 00:08:04 +0530

    Update MaintenanceStatus enums

--- sbms-maintenance-service/src/main/java/com/sbms/sbms_maintenance_service/model/enums/MaintenanceStatus.java
+++ sbms-maintenance-service/src/main/java/com/sbms/sbms_maintenance_service/model/enums/MaintenanceStatus.java
@@ -5,5 +5,10 @@ public enum MaintenanceStatus {
     PENDING,        // submitted by student
     IN_PROGRESS,    // accepted by owner
     COMPLETED,      // fixed
-    REJECTED        // rejected by owner
+    REJECTED,        // rejected by owner
+    ASSIGNED,
+    WORK_DONE,
+    ACCEPTED,
+    CANCELLED,
+   
 }

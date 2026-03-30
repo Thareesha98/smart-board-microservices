7f1afcc4fbc540426596784d118768572cbede45
Thareesha <thareesha98@gmail.com>
2026-03-30 16:23:34 +0530

    Initialize sbms_registration_service

---
+++ sbms_registration_service/src/test/java/com/sbms/sbms_registration_service/SbmsRegistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbms.sbms_registration_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbmsRegistrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
